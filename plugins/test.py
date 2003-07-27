e2252a7a59083c50bde06dfdd56901ee44511c15
Jacek Konieczny <jajcus@jajcus.net>
2003-07-27 07:57:25 +0000

    - /close command for tests

--- plugins/test.py
+++ plugins/test.py
@@ -8,7 +8,61 @@ import time
 from cjc import ui
 from cjc.plugin import PluginBase
 
+class Test(threading.Thread):
+	def __init__(self,plugin,name):
+		threading.Thread.__init__(self,name=name)
+		self.plugin=plugin
+		self.buffer=ui.TextBuffer(self.plugin.cjc.theme_manager,name)
+		self.buffer.register_commands({	"close": (self.cmd_close,
+							"/close",
+							"Closes current test")
+						})
+		self.stop_it=0
+	def cmd_close(self,args):
+		args.finish()
+		self.stop_it=1
+		self.buffer.close()
+
+class ScrollTest(Test): 
+	def __init__(self,plugin):
+		Test.__init__(self,plugin,"Scroll test")
+		
+	def run(self):
+		self.plugin.cjc.info("Test thread started")
+		for i in range(0,200):
+			if self.plugin.cjc.exiting:
+				break
+			if self.stop_it:
+				break
+			self.buffer.append_line("line %i" % (i+1,))
+		self.buffer.update()
+		self.plugin.cjc.info("Test thread finished")
+
+class WrapTest(Test): 
+	def __init__(self,plugin):
+		Test.__init__(self,plugin,"Wrap test")
+		
+	def run(self):
+		self.plugin.cjc.info("Test thread started")
+		for i in range(0,20):
+			for j in range(0,15):
+				if self.stop_it:
+					break
+				if self.plugin.cjc.exiting:
+					break
+				time.sleep(0.1)
+				self.buffer.append("line-%i-word-%i " % (i+1,j+1))
+				self.buffer.update()
+			self.buffer.append_line("")
+			self.buffer.update()
+		self.buffer.update()
+		self.plugin.cjc.info("Test thread finished")
+		
 class Plugin(PluginBase):
+	tests={
+		"scroll": ScrollTest,
+		"wrap": WrapTest
+		}
 	def __init__(self,app):
 		PluginBase.__init__(self,app)
 		app.register_commands({"test": (self.cmd_test,
@@ -21,33 +75,9 @@ class Plugin(PluginBase):
 		if not name:
 			self.cjc.error("Test name not given")
 			return
-		handler=getattr(self,"test_"+name,None)
-		if handler is None:
+		clas=self.tests.get(name,None)
+		if clas is None:
 			self.cjc.error("Uknown test: "+name)
 			return
-		test_thread=threading.Thread(None,self.start_test,name+" test",(handler,args))
+		test_thread=clas(self)
 		test_thread.start()
-
-	def start_test(self,handler,args):
-		self.cjc.info("Test thread started")
-		handler(args)
-		self.cjc.info("Test thread finished")
-
-	def test_scroll(self,args):
-		buf=ui.TextBuffer(self.cjc.theme_manager,"Scroll Test")
-		for i in range(0,200):
-			buf.append_line("line %i" % (i+1,))
-		buf.update()
-
-	def test_wrap(self,args):
-		buf=ui.TextBuffer(self.cjc.theme_manager,"Scroll Test")
-		for i in range(0,20):
-			for j in range(0,15):
-				if self.cjc.exiting:
-					return
-				time.sleep(0.1)
-				buf.append("line-%i-word-%i " % (i+1,j+1))
-				buf.update()
-			buf.append_line("")
-			buf.update()
-		buf.update()
