9728e056cadd5c68426d89e6effe387f956a6f65
Jacek Konieczny <jajcus@jajcus.net>
2003-10-09 10:07:18 +0000

    - new plugin, implements /shell /pipe_in and /pipe_out commands

---
+++ plugins/shell.py
@@ -0,0 +1,77 @@
+
+import os
+from cjc.plugin import PluginBase
+
+class Plugin(PluginBase):
+	def __init__(self,app):
+		PluginBase.__init__(self,app)
+		app.register_commands({"shell": (self.cmd_shell,
+						"/shell command [arg...]",
+						"Executes given shelll command"),
+					"pipe_in": (self.cmd_pipe_in,
+						"/pipe_in command [arg...]",
+						"Takes shell command output as user input"),
+					"pipe_out": (self.cmd_pipe_out,
+						"/pipe_out command [arg...]",
+						"Feeds shell command with current buffer content"),
+					})
+
+	def command_returned(self,command,ret):
+		if ret:
+			es=os.WEXITSTATUS(ret)
+			if not os.WIFEXITED(ret):
+				self.cjc.error("Command exited abnormally")
+			elif es:
+				self.cjc.warning("Command exited with status %i" % (es,))
+
+	def cmd_shell(self,args):
+		command=args.all()
+		try:
+			ret=os.system(command)
+		except OSError,e:
+			self.error("Shell command execution failed: %s" % (e,)) 
+		self.command_returned(command,ret)
+
+	def cmd_pipe_in(self,args):
+		command=args.all()
+		try:
+			pipe=os.popen(command,"r")
+		except OSError,e:
+			self.error("Shell command execution failed: %s" % (e,))
+			return
+		try:
+			try:
+				while 1:
+					l=pipe.readline()
+					if not l:
+						break
+					if l.endswith("\n"):
+						l=l[:-1]
+					if not l or l[0] in ("/","\\"):
+						l="\\"+l
+					l=unicode(l,self.cjc.screen.encoding,"replace")
+					self.cjc.screen.do_user_input(l)
+			except (OSError,IOError),e:
+				self.error("Pipe read failed: %s" % (e,))
+		finally:
+			ret=pipe.close()
+			if ret:
+				self.command_returned(command,ret)
+
+	def cmd_pipe_out(self,args):
+		command=args.all()
+		try:
+			pipe=os.popen(command,"w")
+		except OSError,e:
+			self.error("Shell command execution failed: %s" % (e,))
+			return
+		try:
+			try:
+				if self.cjc.screen.active_window.buffer:
+					pipe.write(self.cjc.screen.active_window.buffer.as_string())
+			except (OSError,IOError),e:
+				self.error("Pipe read failed: %s" % (e,))
+		finally:
+			ret=pipe.close()
+			if ret:
+				self.command_returned(command,ret)
