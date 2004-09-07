affbb3489e8e04aa35cb7e0e3e83ead13298684c
Jacek Konieczny <jajcus@jajcus.net>
2004-09-07 20:13:19 +0000

    - allow applications started with /shell, /pipe_in, /pipe_out to use terminal not breaking CJC display. Use -noterm to force no-terminal mode

--- plugins/shell.py
+++ plugins/shell.py
@@ -28,80 +28,127 @@ class Plugin(PluginBase):
         if ret:
             es=os.WEXITSTATUS(ret)
             if not os.WIFEXITED(ret):
-                self.cjc.error("Command exited abnormally")
+                self.error("Command exited abnormally")
             elif es:
-                self.cjc.warning("Command exited with status %i" % (es,))
+                self.warning("Command exited with status %i" % (es,))
 
     def cmd_shell(self,args):
+        if args.get()=="-noterm":
+            args.shift()
+            term=False
+        else:
+            term=True
         command=args.all()
         if not command or not command.strip():
             self.error("Command must be given")
             return
         try:
-            ret=os.system(command)
+            if term:
+                self.cjc.screen.shell_mode()
+                try:
+                    ret=os.system(command)
+                finally:
+                    self.cjc.screen.prog_mode()
+            else:
+                ret=os.system(command+" 0</dev/null >/dev/null 2>&1")
         except OSError,e:
             self.error("Shell command execution failed: %s" % (e,))
         self.command_returned(command,ret)
 
     def cmd_pipe_in(self,args):
+        if args.get()=="-noterm":
+            args.shift()
+            term=False
+        else:
+            term=True
         command=args.all()
         if not command or not command.strip():
             self.error("Command must be given")
             return
-        try:
-            pipe=os.popen(command,"r")
-        except OSError,e:
-            self.error("Shell command execution failed: %s" % (e,))
-            return
+        if term:
+            self.cjc.screen.shell_mode()
+        else:
+            command+=" 0</dev/null 2>&1"
         try:
             try:
-                while 1:
-                    l=pipe.readline()
-                    if not l:
-                        break
-                    if l.endswith("\n"):
-                        l=l[:-1]
-                    if not l or l[0] in ("/","\\"):
-                        l="\\"+l
-                    l=unicode(l,self.cjc.screen.encoding,"replace")
-                    self.cjc.screen.do_user_input(l)
-            except (OSError,IOError),e:
-                self.error("Pipe read failed: %s" % (e,))
+                pipe=os.popen(command,"r")
+            except OSError,e:
+                self.error("Shell command execution failed: %s" % (e,))
+                return
+            try:
+                try:
+                    while 1:
+                        l=pipe.readline()
+                        if not l:
+                            break
+                        if l.endswith("\n"):
+                            l=l[:-1]
+                        if not l or l[0] in ("/","\\"):
+                            l="\\"+l
+                        l=unicode(l,self.cjc.screen.encoding,"replace")
+                        self.cjc.screen.do_user_input(l)
+                except (OSError,IOError),e:
+                    self.error("Pipe read failed: %s" % (e,))
+            finally:
+                ret=pipe.close()
+                if ret:
+                    self.command_returned(command,ret)
         finally:
-            ret=pipe.close()
-            if ret:
-                self.command_returned(command,ret)
+            if term:
+                self.cjc.screen.prog_mode()
 
     def cmd_pipe_out(self,args):
+        if args.get()=="-noterm":
+            args.shift()
+            term=False
+        else:
+            term=True
         command=args.all()
         if not command or not command.strip():
             self.error("Command must be given")
             return
-        try:
-            pipe=os.popen(command,"w")
-        except OSError,e:
-            self.error("Shell command execution failed: %s" % (e,))
-            return
+        if term:
+            self.cjc.screen.shell_mode()
+        else:
+            command+=" >/dev/null 2>&1"
         try:
             try:
-                if self.cjc.screen.active_window.buffer:
-                    pipe.write(self.cjc.screen.active_window.buffer.as_string())
-            except (OSError,IOError),e:
-                self.error("Pipe read failed: %s" % (e,))
+                pipe=os.popen(command,"w")
+            except OSError,e:
+                self.error("Shell command execution failed: %s" % (e,))
+                return
+            try:
+                try:
+                    if self.cjc.screen.active_window.buffer:
+                        pipe.write(self.cjc.screen.active_window.buffer.as_string())
+                except (OSError,IOError),e:
+                    self.error("Pipe read failed: %s" % (e,))
+            finally:
+                ret=pipe.close()
+                if ret:
+                    self.command_returned(command,ret)
         finally:
-            ret=pipe.close()
-            if ret:
-                self.command_returned(command,ret)
+            if term:
+                self.cjc.screen.prog_mode()
 
 ui.CommandTable("shell",50,(
     ui.Command("shell",Plugin.cmd_shell,
-        "/shell command [arg...]",
-        "Executes given shelll command"),
+        "/shell [-noterm] command [arg...]",
+        "Executes given shelll command. If -noterm option is used"
+        " then the command is not given access to the terminal and the screen"
+        " is not refreshed after command exits.",
+        ("-noterm","opaque")),
     ui.Command("pipe_in",Plugin.cmd_pipe_in,
         "/pipe_in command [arg...]",
-        "Takes shell command output as user input"),
+        "Takes shell command output as user input. If -noterm option is used"
+        " then the command is not given access to the terminal and the screen"
+        " is not refreshed after command exits.",
+        ("-noterm","opaque")),
     ui.Command("pipe_out",Plugin.cmd_pipe_out,
         "/pipe_out command [arg...]",
-        "Feeds shell command with current buffer content"),
+        "Feeds shell command with current buffer content. If -noterm"
+        " option is used then the command is not given access to the"
+        " terminal and the screen is not refreshed after command exits.",
+        ("-noterm","opaque")),
     )).install()
 # vi: sts=4 et sw=4
